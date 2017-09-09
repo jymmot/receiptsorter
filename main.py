e08dfafc50c5aafc10b1b17e85b3062bb060d6b7
jymmot <tommy.vvms@gmail.com>
2017-09-09 13:01:43 -0400

    commit

--- main.py
+++ main.py
@@ -1,11 +1,32 @@
+words = []
+
 def to_list(filepath):
-    words = []
 
     with open(filepath, 'r') as f:
         for line in f:
             for word in line.split():
-                words.append(word)
-
-    print words
+                if word != (":" or "."):
+                    words.append(word)
 
 to_list('receipt.txt')
+
+phone_number = 0
+
+def find_phone():                       # Finds phone number
+    phone_ind = 0
+    for x in range(0, len(words)):
+        if "tel" or "ph" or "cont" in words[x].lower():
+            try:                        # Test to see if entry is a number
+                word = words[x+1]
+                new_word = int(word[:1])
+                if "-" in word or "." in word:      # Niceties
+                    words[x+1] = word.replace(".","-")
+                else:
+                    if len(words[x+1]) == 10:
+                        words[x+1] = word[:3] + "-" + word[3:6] + "-" + word[6:]
+                phone_ind = x+1
+                break
+            except ValueError:
+                continue
+    global phone_number = words[phone_ind]
+
