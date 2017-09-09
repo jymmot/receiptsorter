7cfd87732029c3f187a20e067b9e63b7cb1834c3
jymmot <tommy.vvms@gmail.com>
2017-09-09 13:21:47 -0400

    phone number

--- main.py
+++ main.py
@@ -1,32 +1,31 @@
-words = []
+class Receipt:
+
+    def __init__(self, filepath):
+        self.filepath = filepath
+
+WORDS = []
 
 def to_list(filepath):
-
     with open(filepath, 'r') as f:
         for line in f:
             for word in line.split():
                 if word != (":" or "."):
-                    words.append(word)
-
-to_list('receipt.txt')
-
-phone_number = 0
+                    WORDS.append(word)
 
 def find_phone():                       # Finds phone number
-    phone_ind = 0
-    for x in range(0, len(words)):
-        if "tel" or "ph" or "cont" in words[x].lower():
+    for x in range(0, len(WORDS)):
+        if "tel" or "ph" or "cont" in WORDS[x].lower():
             try:                        # Test to see if entry is a number
-                word = words[x+1]
+                word = WORDS[x+1]
                 new_word = int(word[:1])
                 if "-" in word or "." in word:      # Niceties
-                    words[x+1] = word.replace(".","-")
+                    WORDS[x+1] = word.replace(".","-")
                 else:
-                    if len(words[x+1]) == 10:
-                        words[x+1] = word[:3] + "-" + word[3:6] + "-" + word[6:]
-                phone_ind = x+1
+                    if len(WORDS[x+1]) == 10:
+                        WORDS[x+1] = word[:3] + "-" + word[3:6] + "-" + word[6:]
+                return WORDS[x+1]
                 break
-            except ValueError:
+            except ValueError:          # If string after tel, ph or cont is actually not an integer
                 continue
-    global phone_number = words[phone_ind]
+
 
