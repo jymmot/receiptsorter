546012b0eb7e90ab3641c986da051b21a4ab1db4
jymmot <tommy.vvms@gmail.com>
2017-09-09 18:11:13 -0400

    commit

--- main.py
+++ main.py
@@ -9,21 +9,17 @@ class Receipt:
 
     def find_phone(self):                       # Finds phone number
         for x in range(0, len(self.words)):
-            for y in range(0, len(self.words[x])-1):
-                if any((c in self.words[x][y].lower()) for c in ["tel", "ph", "cont"]):
-                    try:                    # Test to see if entry is a number
-                        word = self.words[x][y+1]
-                        new_word = int(word[:1])
-                        if "-" in word or "." in word:      # Niceties
-                            self.words[x][y+1] = word.replace(".","-")
-                        else:
-                            if len(self.words[x][y+1]) == 10:
-                                self.words[x][y+1] = word[:3] + "-" + word[3:6] + "-" + word[6:]
-                        temp = self.words[x][y+1]   # Ensures phone number doesn't mess with future operations
-                        self.words[x].pop(y+1)
-                        return temp
-                    except ValueError:          # If string after tel, ph or cont is actually not an integer
-                        continue
+            for y in range(0, len(self.words[x])):
+                if self.words[x][y][0].isdigit():
+                    word = self.words[x][y]
+                    if "-" in word or "." in word:      # Niceties
+                        self.words[x][y] = word.replace(".", "-")
+                    else:
+                        if len(self.words[x][y]) == 10:
+                            self.words[x][y] = word[:3] + "-" + word[3:6] + "-" + word[6:]
+                    temp = self.words[x][y]
+                    self.words[x][y] = " "
+                    return temp
 
     def find_price(self):
         change = False
@@ -37,10 +33,10 @@ class Receipt:
                     continue
                 try:
                     if len(word) >= 3 and word[len(word)-3] == ".":
-                        if word[0:1] == "$":
+                        if word[0] == "$":
                             word = word[1:]
                         costs.append(float(word))
-
+                        self.words[x].remove(word)
                 except ValueError:
                     continue
         
@@ -56,12 +52,31 @@ class Receipt:
             while len(max_cost) - max_cost.index(".") < 3:
                 max_cost += "0"
             return max_cost
+    
+    def find_date(self):
+        for x in range(0, len(self.words)):
+            for y in range(0, len(self.words[x])):
+                word = self.words[x][y]
+                if word[0].isdigit():
+                    if any((c in word) for c in ["/", "-"]):
+                        if word.count("/") == 2 or word.count("-") == 2:
+                            return word
+
+    def list_word(self):
+        word_list = []
+        for x in range(0, len(self.words)):
+            if self.words[x][0][0] == "\"":
+                continue
+            for y in range(0, len(self.words[x])):
+                word_list.append(self.words[x][y])
+        return word_list
 
 receipt = Receipt("receipt.txt")
 receipt.to_list()
 print receipt.find_phone()
 print receipt.find_price()
-
+print receipt.find_date()
+print receipt.list_word()
 
 
 ''' def find_items(self):                       # Finds items with corresponding prices
